02a8b802daf2e4109b52c21fcbc056eff24213ae
SamuelSaquiray <152956443+SamuelSaquiray@users.noreply.github.com>
2025-02-27 10:53:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const twilio = require('twilio');
 const nodemailer = require("nodemailer");
 const ExcelJS = require("exceljs");
 
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,7 +27,7 @@ const receiver_email = "samuel.saquiray@utec.edu.pe";
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://ppheart.netlify.app" }));
 
 // Cargar credenciales de Firebase
 const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
